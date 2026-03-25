452d9d075444851e58c7a735217d3e8cdd307d71
Navreet Kaur <166969655+NavreetKB@users.noreply.github.com>
2026-03-25 17:41:35 +0530

    regular updation

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db= mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"$12345Reet",
+    password:"password12345",
     database:"blog"
 })
